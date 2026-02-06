8c6502fbdb4a848e85ac62e22bcebb2df3429d38
AristosDragatis <dragatis.aris@gmail.com>
2026-02-06 21:36:39 +0200

    add @keyframes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+    /*  we use styling from tailwind */
+    return <div className="min-h-screen ">
+
+    {/*  Theme Toggle  */ }
+
+    {/*  Background  Effects*/ }
+
+    {/*  Navbar */ }
+
+    {/*  Main content*/ }
+
+
+    {/*  Footer*/ }
+    </div>
+}
